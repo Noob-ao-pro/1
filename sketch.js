2e873be737324a334bad61d1bc20ec40878acd07
Noob-ao-pro <108024420+Noob-ao-pro@users.noreply.github.com>
2022-06-22 15:59:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var quadrado;
+
+
+
+
+
+
+function setup() {
+
+  createCanvas(400,400);
+quadrado = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+
+}
+
+
+
+
